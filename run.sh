0d2a0970f151a76acbbe2e721beace26fea00036
Sean Talts <sean.talts@gmail.com>
2018-03-19 16:24:10 -0400

    Better result formatting

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
-echo "# Results" > README.md
 for i in *.cpp; do
     test=${i%.cpp}
     echo $test
     rm $test
     make -j4 $test
-    ./$test --benchmark_out=results.txt --benchmark_out_format=console
-    cat results.txt >> README.md
+    ./${test} --benchmark_out=${test}_results.txt --benchmark_out_format=console
 done
-rm results.txt
